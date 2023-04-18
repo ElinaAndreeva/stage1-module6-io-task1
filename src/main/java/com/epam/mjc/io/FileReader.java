670285134cf43af2aec2bc4f31c33566997272bf
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-18 08:54:37 +0400

    Completed the task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String str;
+            while ((str = reader.readLine()) != null) {
+                int index = str.indexOf(":");
+                if (str.contains("Name"))
+                    profile.setName(str.substring(index + 2));
+                else if (str.contains("Age"))
+                    profile.setAge(Integer.parseInt(str.substring(index + 2)));
+                else if (str.contains("Email"))
+                    profile.setEmail(str.substring(index + 2));
+                else if (str.contains("Phone"))
+                    profile.setPhone(Long.parseLong(str.substring(index + 2)));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile(profile.getName(), profile.getAge(), profile.getEmail(), profile.getPhone());
     }
 }
